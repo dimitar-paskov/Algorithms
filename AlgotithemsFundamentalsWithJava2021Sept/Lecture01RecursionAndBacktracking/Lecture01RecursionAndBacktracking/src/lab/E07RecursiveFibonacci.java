8600b6a962fc7cd7f45aa50acea145636b1c0f39
dimitar_paskov <dimitarp2002@abv.bg>
2022-06-17 22:26:44 +0300

    memoization

--- AlgotithemsFundamentalsWithJava2021Sept/Lecture01RecursionAndBacktracking/Lecture01RecursionAndBacktracking/src/lab/E07RecursiveFibonacci.java
+++ AlgotithemsFundamentalsWithJava2021Sept/Lecture01RecursionAndBacktracking/Lecture01RecursionAndBacktracking/src/lab/E07RecursiveFibonacci.java
@@ -4,6 +4,38 @@
  */
 package lab;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class E07RecursiveFibonacci {
+
+	private static long[] arr;
+
+	private static long fibonacci(int n) {
+		if (n <= 1) {
+			return 1;
+		}
+
+		if (arr[n] != -1) {
+			return arr[n];
+		}
+		
+		long result = fibonacci(n - 1) + fibonacci(n - 2);
+		arr[n] = result;
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+
+		arr = new long[n+1];
+		Arrays.fill(arr, -1);
+
+		System.out.println(fibonacci(n)); // 89
+//	    System.out.println(fibonacci(50)); // This will hang!
+	}
 
 }
